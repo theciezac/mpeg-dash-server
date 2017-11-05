2928a702351cb120dee74e98c9364a6114172f16
Ciezac <kaochuanzhi@gmail.com>
2017-11-06 01:52:31 +0800

    Update table with the new number of transcoded streamlets to the respective columns (LAST_TRANSCODED_STREAMLET_[quality]).

--- updateSqlTranscodeStatus.php
+++ updateSqlTranscodeStatus.php
@@ -1,32 +1,33 @@
 <?php
 // executed command should be
 // php updateSqlTranscodeStatus.php fileName quality idx
-$videoTitle = $argv[1]; 
-$quality = $argv[2];
-$videoTitle = $argv[3];
+// $videoTitle = $argv[1]; 
+// $quality = $argv[2];
+echo "Updating sql transcode status for ".$quality."...";
+//$videoTitle = $argv[3];
 
-$servername = "localhost";
-$username = "team07";
-$password = "cs5248team07";
-$db = "team07";
+// $servername = "localhost";
+// $username = "team07";
+// $password = "cs5248team07";
+// $db = "team07";
 
-$myfile = fopen("updateSqlTranscodephp.log", "w") or die("Unable to open file!");
-fwrite($myfile, "videoTitle: ".$videoTitle."\n");
-fwrite($myfile, "quality: ".$quality."\n");
-fwrite($myfile, "videoTitle: ".$videoTitle."\n");
+$updateLogFile = fopen("updateSqlTranscodephp".$quality.".log", "w+") or die("Unable to open file!");
+fwrite($updateLogFile, "videoTitle: ".$videoTitle."\n");
+fwrite($updateLogFile, "quality: ".$quality."\n");
+fwrite($updateLogFile, "videoTitle: ".$videoTitle."\n");
 
 
 // Create connection
-$conn = mysqli_connect($servername, $username, $password, $db);
+// $conn = mysqli_connect($servername, $username, $password, $db);
 
 // Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} else {
-    echo "Connected to DB.<br/>";
-}
+// if ($conn->connect_error) {
+//     die("Connection failed: " . $conn->connect_error);
+// } else {
+//     echo "Connected to DB.<br/>";
+// }
 
-$findQuery = "SELECT * FROM UPLOAD_VIDEO WHERE VIDEO_TITLE = '" . $videoTitle . "';";
+$updateFindQuery = "SELECT * FROM UPLOAD_VIDEO WHERE VIDEO_TITLE = '" . $videoTitle . "';";
 
 switch ($quality) {
     case "480p":
@@ -41,25 +42,35 @@ switch ($quality) {
     default:
     break;
 }
-$findResult = $conn->query($findQuery);
-if ($findResult->num_rows > 0) {
+
+fwrite($updateLogFile, "transcodeColumn: ".$transcodeColumn."\n");
+
+fwrite($updateLogFile, "Find query:\n".$updateFindQuery."\n");
+
+$updateFindResult = $conn->query($updateFindQuery);
+if ($updateFindResult->num_rows > 0) {
     // Existing record in database, update number of streamlets.
-    $row = $findResult->fetch_assoc();
+    fwrite($updateLogFile, "Find result number of rows: ".$updateFindResult->num_rows."\n");
+    $row = $updateFindResult->fetch_assoc();
     $idx = $row["IDX"];
     $transcodedNumberOfStreamlets = $row[$transcodeColumn];
     $newNumberOfTranscoded = $transcodedNumberOfStreamlets + 1;
 
-    $updateQuery = "UPDATE UPLOAD_VIDEO SET `. $transcodeColumn .` = ". $newNumberOfStreamlets  ." WHERE IDX = ". $idx .";" ;
-    $updateResult = $conn->query($updateQuery);
+    $updateUpdateQuery = "UPDATE UPLOAD_VIDEO SET `$transcodeColumn` = $newNumberOfTranscoded  WHERE IDX = $idx;" ;
+    fwrite($updateLogFile, "Update query: \n".$updateUpdateQuery."\n");
+    $updateUpdateResult = $conn->query($updateUpdateQuery);
 
     echo "Performing update query...";
-    if (!$updateResult) {
+    if (!$updateUpdateResult) {
         echo "Update query failed. Reason: " . $conn->error."<br/>";
+        fwrite ($updateLogFile,"Update query failed. Reason: " . $conn->error."\n");
+        fwrite ($updateLogFile, "Update query:\n" . $updateUpdateQuery);
     } else {
         echo "Updated record successfully.";
+        fwrite($updateLogFile, "Updated successfully.");
     }
 }
 
-mysqli_close($conn);
-fclose($myfile);
+// mysqli_close($conn);
+fclose($updateLogFile);
 ?>
