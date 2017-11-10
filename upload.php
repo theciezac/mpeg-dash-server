6d31b7db5a17b715c2848375bc75a20ef36c1d46
Chuan Zhi K <kaochuanzhi@gmail.com>
2017-11-10 21:44:56 +0800

    Add queries to compare whether video has been completed and available for playing.

--- upload.php
+++ upload.php
@@ -2,24 +2,26 @@
 // PARAMETERS EXPECTED
 // $_POST["deviceId"];
 // $_POST["videoTitle"];
-// $_POST["streamlets"]
+// $_POST["totalStreamlets"]
+// $_POST["streamletNo"];
 // $_FILES["fileToUpload"]["name"])
 
-//header('Content-type: application/json');
+if (empty($_POST["deviceId"]) || empty($_POST["videoTitle"] || empty($_POST["totalStreamlets"] || empty($_POST["streamletNo"])))) {
+    exit("Expecting parameters deviceId, videoTitle, totalStreamlets, streamletNo");
+}
 
+//header('Content-type: application/json');
+define ('SITE_ROOT', realpath(dirname(__FILE__)));
 $servername = "localhost";
 $username = "team07";
 $password = "cs5248team07";
 $db = "team07";
 
-$myfile = fopen("uploadphp.log", "w") or die("Unable to open file!");
+$myfile = fopen("uploadphp.log", "w+") or die("Unable to open file!");
 fwrite($myfile, "deviceId: ".$_POST["deviceId"]."\n");
 fwrite($myfile, "videoTitle: ".$_POST["videoTitle"]."\n");
-fwrite($myfile, "(number of) streamlets: ".$_POST["streamlets"]."\n");
-
-// ini_set ( string $varname , string $newvalue )
-ini_set("upload_max_filesize", "40M");
-ini_set("post_max_size", "40M");
+fwrite($myfile, "Total streamlets: ".$_POST["totalStreamlets"]."\n");
+fwrite($myfile, "Streamlet no: ".$_POST["streamletNo"]."\n");
 
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $db);
@@ -32,7 +34,6 @@ if ($conn->connect_error) {
 } else {
     echo "Connected to DB.<br/>";
 }
-
 
 $videoTitle =  $_POST["videoTitle"];
 
@@ -49,13 +50,7 @@ if (file_exists($target_file)) {
 
     $uploadOk = 0;
 }
-// Check file size
-// if ($_FILES["fileToUpload"]["size"] > 4320000) {
-//     echo "Sorry, your file is too large.<br/>";
-//     error_log("Failed - file size too large", 0);
-//     fwrite($myfile, "Failed - file size too large (Line " . __LINE__ . ")");
-//     $uploadOk = 0;
-// }
+
 // Allow certain file formats
 if($imageFileType == "mp4" || $imageFileType == "m4s" || $imageFileType = "ts") {
     echo "File type: " . $imageFileType . "<br/>";
@@ -74,7 +69,7 @@ if ($uploadOk == 0) {
 
 // if everything is ok, try to upload file
 } else {
-    $file_dir = "video_repo/" . $_POST["videoTitle"];
+    $file_dir = SITE_ROOT."/video_repo/" . $_POST["videoTitle"];
     if (!file_exists($file_dir)) {
         shell_exec ("mkdir " . $file_dir);
         shell_exec ("chmod 777 " . $file_dir);
@@ -88,7 +83,7 @@ if ($uploadOk == 0) {
         shell_exec ("mkdir " . $file_dir . "/480p");
         shell_exec ("chmod 777 " . $file_dir . "/480p");
     }
-    $moveResult = move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file);
+    $moveResult = move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], SITE_ROOT."/".$target_file);
     if ($moveResult) {
         echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.<br/>";
     } else {
@@ -98,12 +93,29 @@ if ($uploadOk == 0) {
     }
 }
 
+// get the size of the output
+$outputSize = ob_get_length();
+
+// send headers to tell the browser to close the connection
+header("Content-Length: $outputSize");
+header('Connection: close');
+
+// Flush all output
+ob_end_flush();
+ob_flush();
+flush();
+
+// close current session
+if (session_id()) session_write_close();
+
+// Start background processes
+
 $findQuery = "SELECT * FROM UPLOAD_VIDEO WHERE VIDEO_TITLE = '" . $_POST["videoTitle"]. "';";
 
 $uploadedNumberOfStreamlets = 0;
 $newNumberOfStreamlets = 0;
 
-$insertQuery = "INSERT INTO UPLOAD_VIDEO (`IDX`, `UPLOAD_DEVICE_ID`, `VIDEO_TITLE`, `TOTAL_NUMBER_OF_STREAMLETS`, `UPLOADED_NUMBER_OF_STREAMLETS`, `LAST_UPLOAD_TIME`, `LAST_TRANSCODED_STREAMLET_240P`, `LAST_TRANSCODED_STREAMLET_360P`, `LAST_TRANSCODED_STREAMLET_480P`) VALUES (NULL, '" . $_POST["deviceId"] . "', '" . $_POST["videoTitle"] . "', " . $_POST["streamlets"] . ", '1', CURRENT_TIMESTAMP, '0', '0', '0');";
+$insertQuery = "INSERT INTO UPLOAD_VIDEO (`IDX`, `UPLOAD_DEVICE_ID`, `VIDEO_TITLE`, `TOTAL_NUMBER_OF_STREAMLETS`, `UPLOADED_NUMBER_OF_STREAMLETS`, `LAST_UPLOAD_TIME`, `LAST_TRANSCODED_STREAMLET_240P`, `LAST_TRANSCODED_STREAMLET_360P`, `LAST_TRANSCODED_STREAMLET_480P`) VALUES (NULL, '" . $_POST["deviceId"] . "', '" . $_POST["videoTitle"] . "', " . $_POST["totalStreamlets"] . ", '1', CURRENT_TIMESTAMP, '0', '0', '0');";
 
 $findResult = $conn->query($findQuery);
 if ($findResult->num_rows > 0) {
@@ -116,56 +128,86 @@ if ($findResult->num_rows > 0) {
     $updateQuery = "UPDATE UPLOAD_VIDEO SET `UPLOADED_NUMBER_OF_STREAMLETS` = ". $newNumberOfStreamlets  ." WHERE IDX = ". $idx .";" ;
     $updateResult = $conn->query($updateQuery);
 
-    echo "Performing update query...";
+    fwrite($myfile, "Performing update query...\n");
     if (!$updateResult) {
-        echo "Update query failed. Reason: " . $conn->error."<br/>";
         error_log("Failed - update query", 0);
         fwrite($myfile, "Failed - update query. Reason: ". $conn->error. " (Line " . __LINE__ . ")\n");
         fwrite($myfile, $updateQuery."\n)");
-        
-
     } else {
-        echo "Updated record successfully.";
+        fwrite($myfile, "Updated SQL record successfully.\n");
     }
 } else {
     // No existing records in database, new video with first streamlet to be stored.
     $insertResult = $conn->query($insertQuery);
-    echo "Performing insert query...";
     if (!$insertResult) {
-        echo "Insert query failed. Reason: ".$conn->error."<br/>";
         fwrite($myfile, "Failed - insert query. Reason: ".$conn->error. " (Line " . __LINE__ . ")\n");
         fwrite($myfile, $insertQuery."\n)");
 
     } else {
-        echo "Added record successfully.";
+        fwrite($myfile, "Added SQL record successfully.\n");
     }
 }
-echo "<br/></p><p>";
 
-$transcodeCommand240p = "nohup ./transcode.sh " . $target_file . " 240p ". $file_dir . " &>> video_repo/" . $_POST["videoTitle"] . "/nohup.out &";
-$transcodeCommand360p = "nohup ./transcode.sh " . $target_file . " 360p ". $file_dir . " &>> video_repo/" . $_POST["videoTitle"] . "/nohup.out &";
-$transcodeCommand480p = "nohup ./transcode.sh " . $target_file . " 480p ". $file_dir . " &>> video_repo/" . $_POST["videoTitle"] . "/nohup.out &";
+$initialAvailableVideos = 0;
+// Retrieve initial count of AVAILABLE_VIDEOS
+$initialAvailableResult = $conn->query("SELECT COUNT(*) FROM AVAILABLE_VIDEOS;");
+if (!$initialAvailableResult) {
+    fwrite($myfile, "Query for inital number of available videos failed.\n");
+} else {
+    while ($row = $initialAvailableResult->fetch_assoc()) {
+        fwrite($myfile, "Total number of available videos: ". $row["COUNT(*)"] . "\n");
+        $initialAvailableVideos = $row["COUNT(*)"];
+    }
+}
 
-$runningtranscoderstr = shell_exec("echo running transcoder...");
-echo $runningtranscoderstr;
+$transcodeCommand240p = SITE_ROOT."/transcode.sh " . $target_file . " 240p ". $file_dir . " ". $_POST["streamletNo"]; 
+$transcodeCommand360p = SITE_ROOT."/transcode.sh " . $target_file . " 360p ". $file_dir . " ". $_POST["streamletNo"];
+$transcodeCommand480p = SITE_ROOT."/transcode.sh " . $target_file . " 480p ". $file_dir . " ". $_POST["streamletNo"];
 
-$e1 = shell_exec($transcodeCommand240p);
-echo $transcodeCommand240p;
-echo $e1;
+$runningtranscoderstr = shell_exec("echo running transcoder...<br/>");
 
-$e2 = shell_exec($transcodeCommand360p);
-echo $transcodeCommand360p;
-echo $e2;
+exec($transcodeCommand240p, $output240p, $status240p);
 
-$e3 = shell_exec($transcodeCommand480p);
-echo $transcodeCommand480p;
-echo $e3;
+fwrite($myfile, "Transcode 240p exit status ".$status240p.", output: ".$output240p[0]."\n");
+if (end($output240p) == "[0][0]") {
+    $quality = "240p";
+    include("updateSqlTranscodeStatus.php");
+}
 
-$createMpdFileCommand =  "php createMpdFile.php " . $_POST["videoTitle"];
+exec($transcodeCommand360p, $output360p, $status360p);
 
-exec($createMpdFileCommand);
+fwrite($myfile, "Transcode 360p exit status ".$status360p.", output: ".$output360p[0]."\n");
+if (end($output360p) == "[0][0]") {
+    $quality = "360p";
+    include("updateSqlTranscodeStatus.php");
+}
 
-echo "</p>";
+exec($transcodeCommand480p, $output480p, $status480p);
+
+fwrite($myfile, "Transcode 480p exit status ".$status480p.", output: ".$output480p[0]."\n");
+if (end($output480p) == "[0][0]") {
+    $quality = "480p";
+    include("updateSqlTranscodeStatus.php");
+}
+
+$finalAvailableVideos = 0;
+// Retrieve final count of AVAILABLE_VIDEOS
+$finalAvailableResult = $conn->query("SELECT COUNT(*) FROM AVAILABLE_VIDEOS;");
+if (!$finalAvailableResult) {
+    fwrite($myfile, "Query for final number of available videos failed.\n");
+} else {
+    while ($row = $finalAvailableResult->fetch_assoc()) {
+        fwrite($myfile, "Total number of available videos: ". $row["COUNT(*)"] . "\n");
+        $finalAvailableVideos = $row["COUNT(*)"];
+    }
+}
+
+if ($finalAvailableVideos - $initialAvailableVideos == 0) {
+    fwrite($myfile, "No change in total number of available videos.\n");
+} else {
+    fwrite($myfile, "New available video. To create playlists...\n");
+}
+
 mysqli_close($conn);
 fclose($myfile);
 
